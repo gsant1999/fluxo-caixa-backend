8fc5ec3e2108dd4bdf4a869cb0a1528ebd26165d
gsant1999 <150278001+gsant1999@users.noreply.github.com>
2025-08-02 23:25:51 -0300

    Delete backend directory

--- backend/app/api/v1/router.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-from app.api.v1 import auth, sales
-
-api_router = APIRouter()
-
-api_router.include_router(auth.router, prefix="/auth", tags=["authentication"])
-api_router.include_router(sales.router, prefix="/sales", tags=["sales"])
-
